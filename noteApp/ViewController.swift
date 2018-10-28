61d2f17774716fc8e4a87c914b755b4a2da718c6
Marcel Harvan <marcel.harvan@gmail.com>
2018-10-28 19:41:34 -0400

    App Init

---
+++ noteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  noteApp
+//
+//  Created by Marcel Harvan on 2018-10-28.
+//  Copyright © 2018 Marcel Harvan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
